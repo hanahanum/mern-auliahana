db7c5b68cd8e7724c9eeae1f88ba27f07edbc056
hanahanum <109431053+hanahanum@users.noreply.github.com>
2022-07-29 08:52:31 +0700

    Add files via upload

---
+++ my-app/src/components/BookCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
+
+const BookCard = (props) => {
+    const  book  = props.book;
+
+    return(
+        <div className="card-container">
+            <img src="https://www.adobe.com/express/create/cover/media_181e3d2c78f153ae7bf0e19a2faeb9a76e234da30.jpeg?width=1500&height=400&format=jpeg&justify-content=center" alt="" />
+            <div className="desc">
+                <h2>
+                    <Link to={`/show-book/${book._id}`}>
+                        { book.title }
+                    </Link>
+                </h2>
+                <h3>{book.author}</h3>
+                <p>{book.description}</p>
+            </div>
+        </div>
+    )
+};
+
+export default BookCard;
